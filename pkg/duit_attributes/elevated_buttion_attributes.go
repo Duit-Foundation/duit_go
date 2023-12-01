b09b4a7fce05ffa7abb11fa93900ecb3dd255976
lesleysin <nikandsin@gmail.com>
2023-12-01 15:14:11 +0200

    Added elevated button widget

---
+++ pkg/duit_attributes/elevated_buttion_attributes.go
@@ -0,0 +1,6 @@
+package duit_attributes
+
+type ElevatedButtonAttributes struct {
+	Autofocus    bool `json:"autofocus,omitempty"`
+	ClipBehavior Clip `json:"clipBehavior,omitempty"`
+}
---
+++ pkg/duit_widget/elevated_button.go
@@ -0,0 +1,10 @@
+package duit_widget
+
+import (
+	"github.com/lesleysin/duit_go/pkg/duit_attributes"
+	"github.com/lesleysin/duit_go/pkg/duit_core"
+)
+
+func ElevatedButtonUiElement(attributes *duit_attributes.ElevatedButtonAttributes, id string, controlled bool, action *duit_core.Action) *duit_core.DuitElementModel {
+	return new(duit_core.DuitElementModel).CreateElement(duit_core.ElevatedButton, id, "", attributes, action, controlled)
+}
