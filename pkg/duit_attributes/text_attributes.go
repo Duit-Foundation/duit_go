c93e44c7cd1f008a134bd9dbefe4a08a4c08ed72
lesleysin <nikandsin@gmail.com>
2023-12-18 15:47:26 +0200

    Remove deprecated APIs

--- pkg/duit_attributes/text_attributes.go
+++ pkg/duit_attributes/text_attributes.go
@@ -6,13 +6,12 @@ import (
 )
 
 type TextAttributes[T duit_color.Color] struct {
-	Data            string                             `json:"data"`
-	SemanticsLabel  string                             `json:"semanticsLabel,omitempty"`
-	TextAlign       duit_text_properties.TextAlign     `json:"textAlign,omitempty"`
-	TextDirection   duit_text_properties.TextDirection `json:"textDirection,omitempty"`
-	TextOverflow    duit_text_properties.TextOverflow  `json:"textOverflow,omitempty"`
-	SoftWrap        bool                               `json:"softWrap,omitempty"`
-	TextScaleFactor float32                            `json:"textScaleFactor,omitempty"`
-	MaxLines        int                                `json:"maxLines,omitempty"`
-	Style           *duit_text_properties.TextStyle[T] `json:"style,omitempty"`
+	Data           string                             `json:"data"`
+	SemanticsLabel string                             `json:"semanticsLabel,omitempty"`
+	TextAlign      duit_text_properties.TextAlign     `json:"textAlign,omitempty"`
+	TextDirection  duit_text_properties.TextDirection `json:"textDirection,omitempty"`
+	TextOverflow   duit_text_properties.TextOverflow  `json:"textOverflow,omitempty"`
+	SoftWrap       bool                               `json:"softWrap,omitempty"`
+	MaxLines       int                                `json:"maxLines,omitempty"`
+	Style          *duit_text_properties.TextStyle[T] `json:"style,omitempty"`
 }
