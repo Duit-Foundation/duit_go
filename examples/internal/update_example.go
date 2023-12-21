7f047dea6592b32e52f7d45577d7b2d9df726a94
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2023-12-21 15:55:58 +0200

    Release v1.1.0

---
+++ examples/internal/update_example.go
@@ -0,0 +1,44 @@
+package internal
+
+import (
+	"fmt"
+
+	"github.com/lesleysin/duit_go/pkg/duit_attributes"
+	"github.com/lesleysin/duit_go/pkg/duit_attributes/duit_color"
+	"github.com/lesleysin/duit_go/pkg/duit_core"
+	"github.com/lesleysin/duit_go/pkg/duit_widget"
+)
+
+func UpdateExample() []byte {
+	var holder duit_core.DuitView
+	builder := holder.Builder()
+
+	root := builder.CreateRootOfExactType(duit_core.Column, nil, "")
+
+	root.AddChildren([]*duit_core.DuitElementModel{
+		duit_widget.TextUiElement[duit_color.ColorString](&duit_attributes.TextAttributes[duit_color.ColorString]{Data: "Layout update example"}, "", false, nil),
+		duit_widget.ElevatedButtonUiElement(&duit_attributes.ElevatedButtonAttributes{}, "button1", true, duit_core.CreateHttpAction("/updateLayout", nil, &duit_core.HttpActionMetainfo{Method: "GET"})).AddChild(
+			duit_widget.TextUiElement[duit_color.ColorString](&duit_attributes.TextAttributes[duit_color.ColorString]{Data: "ElevatedButton"}, "", false, nil),
+		),
+	})
+
+	if value, err := builder.Build(); err != nil {
+		fmt.Println(err.Error())
+		return []byte{}
+	} else {
+		return value
+	}
+}
+
+func UpdatePayload() *duit_core.DuitElementModel {
+	var holder duit_core.DuitView
+	builder := holder.Builder()
+
+	root := builder.CreateRootOfExactType(duit_core.Column, nil, "")
+
+	root.AddChildren([]*duit_core.DuitElementModel{
+		duit_widget.TextUiElement[duit_color.ColorString](&duit_attributes.TextAttributes[duit_color.ColorString]{Data: "Force update!"}, "", false, nil),
+	})
+
+	return root
+}
