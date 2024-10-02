77e984fbf8cb3b3d0be8fe23d433e2b2f0032eaf
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2024-10-02 16:14:23 +0300

    patch: ValueRef model update (#38)

--- pkg/duit_attributes/value_reference_holder.go
+++ pkg/duit_attributes/value_reference_holder.go
@@ -3,12 +3,13 @@ package duit_attributes
 type ValueRef struct {
 	AttributeKey string `json:"attributeKey"`
 	ObjectKey    string `json:"objectKey"`
+	DefaultValue any    `json:"defaultValue,omitempty"`
 }
 
 type ValueReferenceHolder struct {
 	Refs []*ValueRef `json:"refs,omitempty"`
 }
 
-func Ref(attributeKey string, objectKey string) *ValueRef {
-	return &ValueRef{AttributeKey: attributeKey, ObjectKey: objectKey}
+func Ref(attributeKey string, objectKey string, defaultValue any) *ValueRef {
+	return &ValueRef{AttributeKey: attributeKey, ObjectKey: objectKey, DefaultValue: defaultValue}
 }
