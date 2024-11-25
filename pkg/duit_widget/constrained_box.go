747cadf1ff1decd20f961c0a7381705df1ed0537
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2024-11-25 17:17:22 +0200

    feat: ConstrainedBox widget (#40)

---
+++ pkg/duit_widget/constrained_box.go
@@ -0,0 +1,10 @@
+package duit_widget
+
+import (
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_core"
+)
+
+func ConstrainedBox(attributes *duit_attributes.ConstrainedBoxAttributes, id string, controlled bool, child *duit_core.DuitElementModel) *duit_core.DuitElementModel {
+	return new(duit_core.DuitElementModel).CreateElement(duit_core.Column, id, "", attributes, nil, controlled, 2, nil, child)
+}
