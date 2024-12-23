e3f825d0a423d6875aeb3a325a17f2fb63c51e4c
lesleysin <nikandsin@gmail.com>
2024-12-23 13:56:06 +0200

    wip

--- pkg/duit_widget/gesture_detector.go
+++ pkg/duit_widget/gesture_detector.go
@@ -1,9 +1,10 @@
 package duit_widget
 
 import (
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes"
 	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_core"
 )
 
-func GestureDetector[TAction duit_core.Action](attributes *TAction, id string, child *duit_core.DuitElementModel) *duit_core.DuitElementModel {
+func GestureDetector[TAction duit_core.Action](attributes *duit_attributes.GestureDetectorAttributes[TAction], id string, child *duit_core.DuitElementModel) *duit_core.DuitElementModel {
 	return new(duit_core.DuitElementModel).CreateElement(duit_core.GestureDetector, id, "", attributes, nil, true, 1, nil, child)
 }
