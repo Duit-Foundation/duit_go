80551a9f4c88f9ce2887443e7164854b445dcc2b
lesleysin <nikandsin@gmail.com>
2024-12-24 13:18:23 +0200

    Fix opacity

--- pkg/duit_attributes/opacity_attributes.go
+++ pkg/duit_attributes/opacity_attributes.go
@@ -16,8 +16,8 @@ type OpacityAttributes struct {
 func (attributes *OpacityAttributes) MarshalJSON() ([]byte, error) {
 
 	if attributes.Opacity >= 0 && attributes.Opacity <= 1 {
-		return nil, errors.New("opacity must be between 0 and 1")
+		return json.Marshal(attributes)
 	}
 
-	return json.Marshal(attributes)
+	return nil, errors.New("opacity must be between 0 and 1")
 }
