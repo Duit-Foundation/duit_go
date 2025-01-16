434aba0cd798ee39c122f937fdddcdc9eb21f3cc
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-01-16 19:29:04 +0200

    feat: AnimatedOpacity widget (#72)

---
+++ pkg/duit_widget/animated_opacity.go
@@ -0,0 +1,10 @@
+package duit_widget
+
+import (
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_core"
+)
+
+func AnimatedOpacity(attributes *duit_attributes.AnimatedOpacityAttributes, id string, child *duit_core.DuitElementModel) *duit_core.DuitElementModel {
+	return new(duit_core.DuitElementModel).CreateElement(duit_core.AnimatedOpacity, id, "", attributes, nil, true, 1, nil, child)
+}
