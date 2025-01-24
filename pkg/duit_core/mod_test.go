8d262c0989ba4c76d53e6244461e89b3ce12cc7b
lesleysin <nikandsin@gmail.com>
2025-01-24 10:52:43 +0200

    Upd tests

--- pkg/duit_core/mod_test.go
+++ pkg/duit_core/mod_test.go
@@ -7,9 +7,9 @@ import (
 
 func TestBuildWrapper(t *testing.T) {
 	view := DuitView{}
-	builder := view.Builder();
+	builder := view.Builder()
 
-	builder.CreateRoot();
+	builder.CreateRoot()
 
 	j, err := builder.Build()
 
@@ -25,16 +25,16 @@ func TestBuildWrapper(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if (val["root"] == nil) {
+	if val["root"] == nil {
 		t.Fatal("root is nil")
 	}
 }
 
 func TestBuildWithoutWrapper(t *testing.T) {
 	view := DuitView{}
-	builder := view.Builder();
+	builder := view.Builder()
 
-	builder.CreateRoot();
+	builder.CreateRoot()
 
 	j, err := builder.BuildUnwrapped()
 
@@ -50,7 +50,52 @@ func TestBuildWithoutWrapper(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if (val["root"] != nil) {
+	if val["root"] != nil {
 		t.Fatal("root is not nil")
+	}
+}
+
+func TestBuildMultiView(t *testing.T) {
+	view := DuitView{}
+	builder := view.Builder()
+
+	_, err := builder.BuildMultiview()
+
+	if err == nil {
+		t.Fatal("expected error")
+	}
+
+	v1 := &DuitElementModel{
+		ElementType: Text,
+		Attributes:  nil,
+		Controlled:  false,
+		Id:          "v1",
+	}
+
+	v2 := &DuitElementModel{
+		ElementType: Text,
+		Attributes:  nil,
+		Controlled:  false,
+		Id:          "v2",
+	}
+
+	builder.AddWidgets(v1, v2)
+
+	j, err := builder.BuildMultiview()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	val := map[string]interface{}{}
+
+	err = json.Unmarshal(j, &val)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(val["widgets"].([]interface{})) != 2 {
+		t.Fatal("expected 2 widgets")
 	}
 }
