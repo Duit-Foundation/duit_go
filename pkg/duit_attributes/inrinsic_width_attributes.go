6381b1b41134b57e880e607bae63eba0e79b3789
n1k5tas <nikstas056@gmail.com>
2025-02-22 17:37:31 +0500

    add stepWidth, stepHeight

--- pkg/duit_attributes/inrinsic_width_attributes.go
+++ pkg/duit_attributes/inrinsic_width_attributes.go
@@ -1,3 +1,6 @@
 package duit_attributes
 
-type IntrinsicWidthAttributes struct{}
+type IntrinsicWidthAttributes struct {
+	StepWidth  float32 `json:"stepWidth,omitempty"`
+	StepHeight float32 `json:"stepHeight,omitempty"`
+}
