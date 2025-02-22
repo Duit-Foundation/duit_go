a52de17d21ba1b48e1b34d45d922cd640434054d
n1k5tas <nikstas056@gmail.com>
2025-02-22 18:01:47 +0500

    add ValueReferenceHolder AnimatedPropertyOwner

--- pkg/duit_attributes/inrinsic_width_attributes.go
+++ pkg/duit_attributes/inrinsic_width_attributes.go
@@ -1,6 +1,10 @@
 package duit_attributes
 
+import animations "github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_animations"
+
 type IntrinsicWidthAttributes struct {
+	ValueReferenceHolder
+	animations.AnimatedPropertyOwner
 	StepWidth  float32 `json:"stepWidth,omitempty"`
 	StepHeight float32 `json:"stepHeight,omitempty"`
 }
