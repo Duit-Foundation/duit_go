9b04193b20c03d74f63247b6db16cfaedbb19056
eb0KrY4k <30822299+eb0KrY4k@users.noreply.github.com>
2025-02-23 13:42:10 +0500

    patch: fixed textOverflow json tag (#87)

--- pkg/duit_attributes/text_attributes.go
+++ pkg/duit_attributes/text_attributes.go
@@ -13,7 +13,7 @@ type TextAttributes[T duit_color.Color] struct {
 	SemanticsLabel string                             `json:"semanticsLabel,omitempty"`
 	TextAlign      duit_text_properties.TextAlign     `json:"textAlign,omitempty"`
 	TextDirection  duit_text_properties.TextDirection `json:"textDirection,omitempty"`
-	TextOverflow   duit_text_properties.TextOverflow  `json:"textOverflow,omitempty"`
+	Overflow       duit_text_properties.TextOverflow  `json:"overflow,omitempty"`
 	SoftWrap       bool                               `json:"softWrap,omitempty"`
 	MaxLines       int                                `json:"maxLines,omitempty"`
 	Style          *duit_text_properties.TextStyle[T] `json:"style,omitempty"`
