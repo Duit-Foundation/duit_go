5aaaef28b173d2c9eb58aebf7be27a9b2b7712e0
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-03-26 09:47:24 +0200

    Add missing attribute entries

--- pkg/duit_attributes/appbar_attributes.go
+++ pkg/duit_attributes/appbar_attributes.go
@@ -2,6 +2,7 @@ package duit_attributes
 
 import (
 	"encoding/json"
+	"fmt"
 
 	animations "github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_animations"
 	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_clip"
@@ -22,20 +23,22 @@ type AppBarAttributes[TColor duit_color.Color, TInsets duit_edge_insets.EdgeInse
 	Actions                   []*duit_core.DuitElementModel           `json:"actions,omitempty"`
 	FlexibleSpace             *duit_core.DuitElementModel             `json:"flexibleSpace,omitempty"`
 	Bottom                    *duit_core.DuitElementModel             `json:"bottom,omitempty"`
-	TitleStyle                *duit_text_properties.TextStyle[TColor] `json:"titleStyle,omitempty"`
+	TitleTextStyle            *duit_text_properties.TextStyle[TColor] `json:"titleTextStyle,omitempty"`
 	ToolbarTextStyle          *duit_text_properties.TextStyle[TColor] `json:"toolbarTextStyle,omitempty"`
-	Color                     TColor                                  `json:"color,omitempty"`
+	ShadowColor               TColor                                  `json:"shadowColor,omitempty"`
 	BackgroundColor           TColor                                  `json:"backgroundColor,omitempty"`
 	ForegroundColor           TColor                                  `json:"foregroundColor,omitempty"`
 	SurfaceTintColor          TColor                                  `json:"surfaceTintColor,omitempty"`
-	Padding                   TInsets                                 `json:"padding,omitempty"`
+	ActionsPadding            TInsets                                 `json:"actionsPadding,omitempty"`
 	Shape                     *TShape                                 `json:"shape,omitempty"`
 	ClipBehavior              duit_clip.Clip                          `json:"clipBehavior,omitempty"`
 	TitleSpacing              float32                                 `json:"titleSpacing,omitempty"`
 	Elevation                 float32                                 `json:"elevation,omitempty"`
 	ToolbarHeight             float32                                 `json:"toolbarHeight,omitempty"`
 	LeadingWidth              float32                                 `json:"leadingWidth,omitempty"`
-	ScrolledUnderElevation    *bool                                   `json:"scrolledUnderElevation,omitempty"`
+	ScrolledUnderElevation    float32                                 `json:"scrolledUnderElevation,omitempty"`
+	BottomOpacity             float32                                 `json:"bottomOpacity,omitempty"`
+	ToolbarOpacity            float32                                 `json:"toolbarOpacity,omitempty"`
 	ForceMaterialTransparency *bool                                   `json:"forceMaterialTransparency,omitempty"`
 	CenterTitle               *bool                                   `json:"centerTitle,omitempty"`
 	AutomaticallyImplyLeading *bool                                   `json:"automaticallyImplyLeading,omitempty"`
@@ -54,5 +57,13 @@ func (a *AppBarAttributes[TColor, TInsets, TShape]) MarshalJSON() ([]byte, error
 		a.Primary = bPtr
 	}
 
+	if a.ToolbarOpacity < 0 || a.ToolbarOpacity > 1 {
+		return nil, fmt.Errorf("toolbarOpacity must be between 0 and 1")
+	}
+
+	if a.BottomOpacity < 0 || a.BottomOpacity > 1 {
+		return nil, fmt.Errorf("bottomOpacity must be between 0 and 1")
+	}
+	
 	return json.Marshal(*a)
 }
