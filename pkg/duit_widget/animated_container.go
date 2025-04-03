6601f47b4dcf7baa3f348d6a6d154069172eb673
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-04-03 18:45:23 +0200

    feat: AnimatedContainer (#97)

---
+++ pkg/duit_widget/animated_container.go
@@ -0,0 +1,12 @@
+package duit_widget
+
+import (
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_color"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_edge_insets"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_core"
+)
+
+func AnimatedContainer[TInsets duit_edge_insets.EdgeInsets, TColor duit_color.Color](attributes *duit_attributes.AnimatedContainerAttributes[TInsets, TColor], id string, child *duit_core.DuitElementModel) *duit_core.DuitElementModel {
+	return new(duit_core.DuitElementModel).CreateElement(duit_core.AnimatedContainer, id, "", attributes, nil, true, 1, nil, child)
+}
