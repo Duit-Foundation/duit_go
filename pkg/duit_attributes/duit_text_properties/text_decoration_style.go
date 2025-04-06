e5fb7badc7195abc04a68c5c9592667261137b67
fartem <jaman.smlnsk@gmail.com>
2025-04-06 14:11:42 +0300

    patch: added text decoration properties to TextStyle

---
+++ pkg/duit_attributes/duit_text_properties/text_decoration_style.go
@@ -0,0 +1,11 @@
+package duit_text_properties
+
+type TextDecorationStyle string
+
+const (
+	Solid  TextDecorationStyle = "solid"
+	Double TextDecorationStyle = "double"
+	Dotted TextDecorationStyle = "dotted"
+	Dashed TextDecorationStyle = "dashed"
+	Wavy   TextDecorationStyle = "wavy"
+)
--- pkg/duit_attributes/duit_text_properties/text_style.go
+++ pkg/duit_attributes/duit_text_properties/text_style.go
@@ -3,11 +3,15 @@ package duit_text_properties
 import "github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_color"
 
 type TextStyle[T duit_color.Color] struct {
-	Color         T          `json:"color,omitempty"`
-	FontFamily    string     `json:"fontFamily,omitempty"`
-	FontWeight    FontWeight `json:"fontWeight,omitempty"`
-	FontSize      float32    `json:"fontSize,omitempty"`
-	LetterSpacing float32    `json:"letterSpacing,omitempty"`
-	WordSpacing   float32    `json:"wordSpacing,omitempty"`
-	Height        float32    `json:"height,omitempty"`
+	Color               T                   `json:"color,omitempty"`
+	FontFamily          string              `json:"fontFamily,omitempty"`
+	FontWeight          FontWeight          `json:"fontWeight,omitempty"`
+	FontSize            float32             `json:"fontSize,omitempty"`
+	LetterSpacing       float32             `json:"letterSpacing,omitempty"`
+	WordSpacing         float32             `json:"wordSpacing,omitempty"`
+	Height              float32             `json:"height,omitempty"`
+	Decoration          TextDecoration      `json:"decoration,omitempty"`
+	DecorationStyle     TextDecorationStyle `json:"decorationStyle,omitempty"`
+	DecorationColor     T                   `json:"decorationColor,omitempty"`
+	DecorationThickness float32             `json:"decorationThickness,omitempty"`
 }
