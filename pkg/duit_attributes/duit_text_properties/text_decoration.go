32ee6883feb91e461dc6853f7a4447996d2afe5e
Artem Fomchenkov <45129887+fartem@users.noreply.github.com>
2025-04-08 17:13:28 +0300

    patch: added text decoration properties to TextStyle (#98)

---
+++ pkg/duit_attributes/duit_text_properties/text_decoration.go
@@ -0,0 +1,10 @@
+package duit_text_properties
+
+type TextDecoration string
+
+const (
+	None        TextDecoration = "none"
+	Underline   TextDecoration = "underline"
+	Overline    TextDecoration = "overline"
+	LineThrough TextDecoration = "lineThrough"
+)
