a226e8f31f83fb8b7c189fa5961ade7f420351a0
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-04-11 22:43:37 +0200

    Added AnimatedAlign widget

---
+++ pkg/duit_attributes/animated_align.go
@@ -0,0 +1,15 @@
+package duit_attributes
+
+import (
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_alignment"
+	animations "github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_animations"
+)
+
+type AnimatedAlignAttributes struct {
+	ValueReferenceHolder
+	animations.ImplicitAnimatable
+	ThemeConsumer
+	Alignment    duit_alignment.Alignment `json:"alignment,omitempty"`
+	WidthFactor  float32                  `json:"widthFactor,omitempty"`
+	HeightFactor float32                  `json:"heightFactor,omitempty"`
+}
---
+++ pkg/duit_widget/animated_align.go
@@ -0,0 +1,23 @@
+package duit_widget
+
+import (
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_core"
+)
+
+/*
+Example:
+	AnimatedAlign(
+		&duit_attributes.AnimatedAlignAttributes{
+			Alignment: duit_alignment.Center,
+			ImplicitAnimatable: duit_animations.ImplicitAnimatable{
+				Duration: 1000,
+			},
+		},
+		"id",
+		nil, //child
+	)
+*/
+func AnimatedAlign(attributes *duit_attributes.AnimatedAlignAttributes, id string, child *duit_core.DuitElementModel) *duit_core.DuitElementModel {
+	return new(duit_core.DuitElementModel).CreateElement(duit_core.AnimatedAlign, id, "", attributes, nil, true, 1, nil, child)
+}
