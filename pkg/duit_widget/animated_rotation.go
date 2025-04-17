9e0a9ae285449a5c7b523a0ed802d19cf652d7aa
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-04-17 22:03:21 +0200

    wip

--- pkg/duit_widget/animated_rotation.go
+++ pkg/duit_widget/animated_rotation.go
@@ -5,6 +5,19 @@ import (
 	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_core"
 )
 
+/*
+Example:
+	AnimatedRotation(
+		&duit_attributes.AnimatedRotationAttributes{
+			Turns: 3.14,
+			ImplicitAnimatable: duit_animations.ImplicitAnimatable{
+				Duration: 1000,
+			},
+		},
+		"id",
+		nil, //child
+	)
+*/
 func AnimatedRotation(attributes *duit_attributes.AnimatedRotationAttributes, id string, child *duit_core.DuitElementModel) *duit_core.DuitElementModel {
 	return new(duit_core.DuitElementModel).CreateElement(duit_core.AnimatedRotation, id, "", attributes, nil, true, 1, nil, child)
 }
