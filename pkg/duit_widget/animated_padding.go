959c4f206e234744cb5ff324fbee970de8e3e948
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-04-20 11:29:22 +0200

    added example

--- pkg/duit_widget/animated_padding.go
+++ pkg/duit_widget/animated_padding.go
@@ -6,6 +6,19 @@ import (
 	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_core"
 )
 
+/*
+Example:
+	AnimatedPadding(
+		&duit_attributes.AnimatedPaddingAttributes[duit_edge_insets.EdgeInsetsAll]{
+			Padding: 12.0,
+			ImplicitAnimatable: duit_animations.ImplicitAnimatable{
+				Duration: 1000,
+			},
+		},
+		"id",
+		nil, //child
+	)
+*/
 func AnimatedPadding[TInsets duit_edge_insets.EdgeInsets](attributes *duit_attributes.AnimatedPaddingAttributes[TInsets], id string, child *duit_core.DuitElementModel) *duit_core.DuitElementModel {
 	return new(duit_core.DuitElementModel).CreateElement(duit_core.AnimatedPadding, id, "", attributes, nil, true, 1, nil, child)
 }
