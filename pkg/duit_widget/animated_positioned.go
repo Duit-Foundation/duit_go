679d6e92836c408a7b781b7fbb1c3f9e92b432b6
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-04-23 21:51:54 +0200

    wip

--- pkg/duit_widget/animated_positioned.go
+++ pkg/duit_widget/animated_positioned.go
@@ -5,6 +5,20 @@ import (
 	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_core"
 )
 
+/*
+Example:
+	AnimatedPositioned(
+		&duit_attributes.AnimatedPositionedAttributes{
+			Top: 0,
+			Right: 0,
+			ImplicitAnimatable: duit_animations.ImplicitAnimatable{
+				Duration: 1000,
+			},
+		},
+		"id",
+		nil, //child
+	)
+*/
 func AnimatedPositioned(attributes *duit_attributes.AnimatedPositionedAttributes, id string, child *duit_core.DuitElementModel) *duit_core.DuitElementModel {
 	return new(duit_core.DuitElementModel).CreateElement(duit_core.AnimatedPositioned, id, "", attributes, nil, true, 1, nil, child)
 }
