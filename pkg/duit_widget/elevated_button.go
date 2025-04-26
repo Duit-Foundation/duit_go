61aca14dc471e964cdafbbf972b5c155b0b18549
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-04-26 12:21:38 +0200

    Added missing ElevatedButton props

--- pkg/duit_widget/elevated_button.go
+++ pkg/duit_widget/elevated_button.go
@@ -2,9 +2,12 @@ package duit_widget
 
 import (
 	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_color"
+	duit_decoration "github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_decorations"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_edge_insets"
 	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_core"
 )
 
-func ElevatedButton[TAction duit_core.Action](attributes *duit_attributes.ElevatedButtonAttributes, id string, action *TAction, child *duit_core.DuitElementModel) *duit_core.DuitElementModel {
+func ElevatedButton[TColor duit_color.Color, TInsets duit_edge_insets.EdgeInsets, TShape duit_decoration.ShapeBorder[TColor], TAction duit_core.Action](attributes *duit_attributes.ElevatedButtonAttributes[TColor, TInsets, TShape, TAction], id string, action *TAction, child *duit_core.DuitElementModel) *duit_core.DuitElementModel {
 	return new(duit_core.DuitElementModel).CreateElement(duit_core.ElevatedButton, id, "", attributes, action, true, 1, nil, child)
 }
