0c427621c94cf7176d9636562eb29e3ad9093077
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-04-26 12:22:48 +0200

    patch: Added missing ElevatedButton props (#105)

---
+++ pkg/duit_attributes/duit_material/button_style.go
@@ -0,0 +1,28 @@
+package duit_material
+
+import (
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_color"
+	duit_decoration "github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_decorations"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_edge_insets"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_flex"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_text_properties"
+)
+
+type ButtonStyle[TColor duit_color.Color, TInsets duit_edge_insets.EdgeInsets, TShape duit_decoration.ShapeBorder[TColor]] struct {
+	TextStyle         MaterialStateProperty[duit_text_properties.TextStyle[TColor]] `json:"textStyle,omitempty"`
+	BackgroundColor   MaterialStateProperty[TColor]                                 `json:"backgroundColor,omitempty"`
+	ForegroundColor   MaterialStateProperty[TColor]                                 `json:"foregroundColor,omitempty"`
+	OverlayColor      MaterialStateProperty[TColor]                                 `json:"overlayColor,omitempty"`
+	ShadowColor       MaterialStateProperty[TColor]                                 `json:"shadowColor,omitempty"`
+	IconColor         MaterialStateProperty[TColor]                                 `json:"iconColor,omitempty"`
+	Elevation         MaterialStateProperty[float32]                                `json:"elevation,omitempty"`
+	IconSize          MaterialStateProperty[float32]                                `json:"iconSize,omitempty"`
+	Padding           MaterialStateProperty[TInsets]                                `json:"padding,omitempty"`
+	MinimumSize       MaterialStateProperty[duit_flex.Size]                         `json:"minimumSize,omitempty"`
+	MaximumSize       MaterialStateProperty[duit_flex.Size]                         `json:"maximumSize,omitempty"`
+	Side              MaterialStateProperty[duit_decoration.BorderSide[TColor]]     `json:"side,omitempty"`
+	Shape             MaterialStateProperty[TShape]                                 `json:"shape,omitempty"`
+	VisualDensity     MaterialStateProperty[duit_flex.VisualDensity]                `json:"visualDensity,omitempty"`
+	TapTargetSize     MaterialTapTargetSize                                         `json:"materialTapTargetSize,omitempty"`
+	AnimationDuration uint                                                          `json:"animationDuration,omitempty"`
+}
--- pkg/duit_attributes/elevated_buttion_attributes.go
+++ pkg/duit_attributes/elevated_buttion_attributes.go
@@ -1,10 +1,19 @@
 package duit_attributes
 
-import "github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_clip"
+import (
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_clip"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_color"
+	duit_decoration "github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_decorations"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_edge_insets"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_material"
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_core"
+)
 
-type ElevatedButtonAttributes struct {
+type ElevatedButtonAttributes[TColor duit_color.Color, TInsets duit_edge_insets.EdgeInsets, TShape duit_decoration.ShapeBorder[TColor], TAction duit_core.Action] struct {
 	ValueReferenceHolder
 	ThemeConsumer
-	Autofocus    bool           `json:"autofocus,omitempty"`
-	ClipBehavior duit_clip.Clip `json:"clipBehavior,omitempty"`
+	Autofocus    bool                                                `json:"autofocus,omitempty"`
+	ClipBehavior duit_clip.Clip                                      `json:"clipBehavior,omitempty"`
+	Style        *duit_material.ButtonStyle[TColor, TInsets, TShape] `json:"style,omitempty"`
+	OnLongPress  *TAction                                            `json:"onLongPress,omitempty"`
 }
