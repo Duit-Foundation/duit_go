1620a5cc12c4fd07e4cbc4ced158e6575ec2e162
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-04-26 18:31:38 +0200

    patch: passing Scaffold body property through widget child instead of attributes(#106)

--- pkg/duit_widget/scaffold.go
+++ pkg/duit_widget/scaffold.go
@@ -6,6 +6,6 @@ import (
 	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_core"
 )
 
-func Scaffold[TColor duit_color.Color](attributes *duit_attributes.ScaffoldAttributes[TColor], id string) *duit_core.DuitElementModel {
-	return new(duit_core.DuitElementModel).CreateElement(duit_core.Scaffold, id, "", attributes, nil, true, 0, nil)
+func Scaffold[TColor duit_color.Color](attributes *duit_attributes.ScaffoldAttributes[TColor], id string, child *duit_core.DuitElementModel) *duit_core.DuitElementModel {
+	return new(duit_core.DuitElementModel).CreateElement(duit_core.Scaffold, id, "", attributes, nil, true, 1, child)
 }
