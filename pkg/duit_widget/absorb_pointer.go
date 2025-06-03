f76241082e558d962476244ea012158a7c15011b
Mr. KrY4k <nikstas056@gmail.com>
2025-06-03 22:41:31 +0500

    fix AbsorbPointer

--- pkg/duit_widget/absorb_pointer.go
+++ pkg/duit_widget/absorb_pointer.go
@@ -5,6 +5,6 @@ import (
 	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes"
 )
 
-func AbsorbPointer(attributes *duit_attributes.AbsorbPointerAttributes, id string, child *duit_core.DuitElementType) *duit_core.DuitElementModel {
+func AbsorbPointer(attributes *duit_attributes.AbsorbPointerAttributes, id string, child *duit_core.DuitElementModel) *duit_core.DuitElementModel {
 	return new(duit_core.DuitElementModel).CreateElement(duit_core.AbsorbPointer, id, "", attributes, nil, false, 1, nil, child)
 }
