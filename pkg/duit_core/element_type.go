77005612723acaa75b970f95532e557eff5c8ce0
Stanislav Nikolaev <30822299+Mr-KrY4k@users.noreply.github.com>
2025-06-04 12:18:24 +0500

    feat: Offstage (#112)
    * add offstage widget
    
    * remove AnimatedPropertyOwner

--- pkg/duit_core/element_type.go
+++ pkg/duit_core/element_type.go
@@ -66,4 +66,5 @@ const (
 	AnimatedScale          DuitElementType = "AnimatedScale"
 	CustomScrollView       DuitElementType = "CustomScrollView"
 	SliverPadding          DuitElementType = "SliverPadding"
+	Offstage               DuitElementType = "Offstage"
 )
