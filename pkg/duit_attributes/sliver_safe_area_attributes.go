cf55132c5923250b29df213d36b2a4422309798f
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-06-19 13:17:38 +0200

    feat: SliverSafeArea

---
+++ pkg/duit_attributes/sliver_safe_area_attributes.go
@@ -0,0 +1,17 @@
+package duit_attributes
+
+import (
+	"github.com/Duit-Foundation/duit_go/v3/pkg/duit_attributes/duit_edge_insets"
+)
+
+type SliverSafeAreaAttributes[TInsets duit_edge_insets.EdgeInsets] struct {
+	ValueReferenceHolder
+	ThemeConsumer
+	SliverProps
+	Left                      *bool   `json:"left,omitempty"`
+	Top                       *bool   `json:"top,omitempty"`
+	Right                     *bool   `json:"right,omitempty"`
+	Bottom                    *bool   `json:"bottom,omitempty"`
+	Minimum                   TInsets `json:"minimum,omitempty"`
+	MaintainBottomViewPadding *bool   `json:"maintainBottomViewPadding,omitempty"`
+}
--- pkg/duit_core/element_type.go
+++ pkg/duit_core/element_type.go
@@ -72,4 +72,5 @@ const (
 	SliverVisibility       DuitElementType = "SliverVisibility"
 	SliverOpacity          DuitElementType = "SliverOpacity"
 	SliverAnimatedOpacity  DuitElementType = "SliverAnimatedOpacity"
+	SliverSafeArea         DuitElementType = "SliverSafeArea"
 )
