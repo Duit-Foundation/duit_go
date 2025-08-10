1194aa71464a3c24176990795c868f4d5748c55d
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-08-10 15:04:03 +0200

    feat: Fragment (#129)
    * feat: added Fragment element
    
    * Added test

---
+++ pkg/duit_widget/fragment.go
@@ -0,0 +1,21 @@
+package duit_widget
+
+import "github.com/Duit-Foundation/duit_go/v3/pkg/duit_core"
+
+// Fragment creates a Fragment widget element.
+//
+// The parameter tag is required and must be a non-empty string. The id parameter
+// is optional and may be an empty string.
+//
+// *If tag is empty the function panics!*
+//
+// Example:
+//  f := Fragment("profile", "profile-1")
+//  _ = f
+func Fragment(tag string, id string) *duit_core.DuitElementModel {
+	if tag == "" {
+		panic("Tag is required in Fragment definition")
+	}
+
+	return new(duit_core.DuitElementModel).CreateElement(duit_core.Fragment, id, tag, nil, nil, false, 0, nil, nil)
+}
