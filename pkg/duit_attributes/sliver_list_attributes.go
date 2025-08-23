62b024a176d2c1c051023c8774b30aba604f0ff1
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-08-23 19:11:32 +0200

    wip

--- pkg/duit_attributes/sliver_list_attributes.go
+++ pkg/duit_attributes/sliver_list_attributes.go
@@ -1,8 +1,11 @@
 package duit_attributes
 
 import (
+	"errors"
+
 	"github.com/Duit-Foundation/duit_go/v4/pkg/duit_attributes/duit_builder"
 	"github.com/Duit-Foundation/duit_go/v4/pkg/duit_core"
+	"github.com/Duit-Foundation/duit_go/v4/pkg/duit_utils"
 )
 
 type SliverListKind uint8
@@ -14,22 +17,62 @@ const (
 )
 
 type SliverList struct {
-	Type                   SliverListKind `json:"type"`
-	AddAutomaticKeepAlives *bool           `json:"addAutomaticKeepAlives,omitempty"`
-	AddRepaintBoundaries   *bool           `json:"addRepaintBoundaries,omitempty"`
-	AddSemanticIndexes     *bool           `json:"addSemanticIndexes,omitempty"`
+	Type                   duit_utils.Tristate[SliverListKind] `json:"type"`
+	AddAutomaticKeepAlives duit_utils.Tristate[bool]           `json:"addAutomaticKeepAlives,omitempty"`
+	AddRepaintBoundaries   duit_utils.Tristate[bool]           `json:"addRepaintBoundaries,omitempty"`
+	AddSemanticIndexes     duit_utils.Tristate[bool]           `json:"addSemanticIndexes,omitempty"`
+}
+
+func (r *SliverList) Validate() error {
+	if r.Type == nil {
+		return errors.New("type is required")
+	}
+
+	return nil
 }
 
 type SliverListBuilderAttributes struct {
-	SliverList
-	duit_builder.Builder
+	*SliverList
+	*duit_builder.Builder
+}
+
+func (r *SliverListBuilderAttributes) Validate() error {
+	if r.SliverList == nil {
+		return errors.New("sliverList is required")
+	} else {
+		if *r.SliverList.Type != SliverListBuilder {
+			return errors.New("type must be SliverListBuilder")
+		}
+	}
+
+	if r.Builder == nil {
+		return errors.New("builder is required")
+	}
+
+	return nil
 }
 
 type SliverListSeparatedAttributes struct {
-	SliverListBuilderAttributes
+	*SliverListBuilderAttributes
 	Separator *duit_core.DuitElementModel `json:"separator"`
+}
+
+func (r *SliverListSeparatedAttributes) Validate() error {
+	if r.SliverListBuilderAttributes == nil {
+		return errors.New("sliverList is required")
+	} else {
+		if *r.SliverList.Type != SliverListBuilder {
+			return errors.New("type must be SliverListBuilder")
+		}
+	}
+
+	if r.Separator == nil {
+		return errors.New("separator property is required")
+	}
+
+	return nil
 }
 
 type SliverListAttributes interface {
 	SliverList | SliverListBuilderAttributes | SliverListSeparatedAttributes
-} 
+}
