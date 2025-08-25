bbd52639b581414e0045acad71d41b8422366dc8
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-08-25 09:43:45 +0200

    rm unused props from attributes struct

--- pkg/duit_attributes/scaffold_attributes.go
+++ pkg/duit_attributes/scaffold_attributes.go
@@ -4,18 +4,12 @@ import (
 	"github.com/Duit-Foundation/duit_go/v4/pkg/duit_attributes/duit_alignment"
 	"github.com/Duit-Foundation/duit_go/v4/pkg/duit_attributes/duit_color"
 	"github.com/Duit-Foundation/duit_go/v4/pkg/duit_attributes/duit_material"
-	"github.com/Duit-Foundation/duit_go/v4/pkg/duit_core"
 	"github.com/Duit-Foundation/duit_go/v4/pkg/duit_utils"
 )
 
 type ScaffoldAttributes[TColor duit_color.Color] struct {
 	*ValueReferenceHolder
 	*ThemeConsumer
-	AppBar                       *duit_core.DuitElementModel   `json:"appBar,omitempty"`
-	BottomNavigationBar          *duit_core.DuitElementModel   `json:"bottomNavigationBar,omitempty"`
-	BottomSheet                  *duit_core.DuitElementModel   `json:"bottomSheet,omitempty"`
-	FloatingActionButton         *duit_core.DuitElementModel   `json:"floatingActionButton,omitempty"`
-	PersistentFooterButtons      []*duit_core.DuitElementModel `json:"persistentFooterButtons,omitempty"`
 	PersistentFooterAlignment    duit_alignment.Alignment      `json:"persistentFooterAlignment,omitempty"`
 	Primary                      duit_utils.Tristate[bool]     `json:"primary,omitempty"`
 	ExtendBody                   duit_utils.Tristate[bool]     `json:"extendBody,omitempty"`
