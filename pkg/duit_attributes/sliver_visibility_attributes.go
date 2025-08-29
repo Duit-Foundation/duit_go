3e7ad1d64e29dc63b0bc83b5fb6976791a0203c5
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-08-29 21:10:17 +0200

    breaking: SliverVisibility update (#132)

--- pkg/duit_attributes/sliver_visibility_attributes.go
+++ pkg/duit_attributes/sliver_visibility_attributes.go
@@ -3,7 +3,6 @@ package duit_attributes
 import (
 	"errors"
 
-	"github.com/Duit-Foundation/duit_go/v4/pkg/duit_core"
 	"github.com/Duit-Foundation/duit_go/v4/pkg/duit_utils"
 )
 
@@ -11,13 +10,12 @@ type SliverVisibilityAttributes struct {
 	*ValueReferenceHolder
 	*ThemeConsumer
 	*SliverProps
-	Visible               duit_utils.Tristate[bool]   `json:"visible,omitempty"`
-	MaintainState         duit_utils.Tristate[bool]   `json:"maintainState,omitempty"`
-	MaintainAnimation     duit_utils.Tristate[bool]   `json:"maintainAnimation,omitempty"`
-	MaintainSize          duit_utils.Tristate[bool]   `json:"maintainSize,omitempty"`
-	MaintainSemantics     duit_utils.Tristate[bool]   `json:"maintainSemantics,omitempty"`
-	MaintainInteractivity duit_utils.Tristate[bool]   `json:"maintainInteractivity,omitempty"`
-	ReplacementSliver     *duit_core.DuitElementModel `json:"replacementSliver,omitempty"`
+	Visible               duit_utils.Tristate[bool] `json:"visible,omitempty"`
+	MaintainState         duit_utils.Tristate[bool] `json:"maintainState,omitempty"`
+	MaintainAnimation     duit_utils.Tristate[bool] `json:"maintainAnimation,omitempty"`
+	MaintainSize          duit_utils.Tristate[bool] `json:"maintainSize,omitempty"`
+	MaintainSemantics     duit_utils.Tristate[bool] `json:"maintainSemantics,omitempty"`
+	MaintainInteractivity duit_utils.Tristate[bool] `json:"maintainInteractivity,omitempty"`
 }
 
 func (r *SliverVisibilityAttributes) Validate() error {
@@ -27,6 +25,10 @@ func (r *SliverVisibilityAttributes) Validate() error {
 
 	if err := r.ThemeConsumer.Validate(); err != nil {
 		return err
+	}
+
+	if r.Visible == nil {
+		return errors.New("visible property is required")
 	}
 
 	if r.Visible == nil {
