8561ed02bd786b08705aaf0dee450827a1490cb5
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-09-09 16:19:05 +0200

    Fixed Wrap widget children param

--- pkg/duit_widget/wrap.go
+++ pkg/duit_widget/wrap.go
@@ -5,6 +5,6 @@ import (
 	"github.com/Duit-Foundation/duit_go/v4/pkg/duit_core"
 )
 
-func Wrap(attributes *duit_attributes.WrapAttributes, id string, controlled bool, child *duit_core.DuitElementModel) *duit_core.DuitElementModel {
-	return new(duit_core.DuitElementModel).CreateElement(duit_core.Wrap, id, "", attributes, nil, controlled, 2, nil, child)
+func Wrap(attributes *duit_attributes.WrapAttributes, id string, controlled bool, children []*duit_core.DuitElementModel) *duit_core.DuitElementModel {
+	return new(duit_core.DuitElementModel).CreateElement(duit_core.Wrap, id, "", attributes, nil, controlled, 2, nil, children...)
 }
